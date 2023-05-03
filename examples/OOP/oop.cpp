d593c81c5d7fb300635aa51d17205cfcf9ebbafd
wangwei <1061124083@qq.com>
2023-05-03 16:04:51 +0800

    add oop example

--- examples/OOP/oop.cpp
+++ examples/OOP/oop.cpp
@@ -3,11 +3,26 @@
 class Base
 {
 public:
-   Base() {}
+    Base() { std::cout << "Base constructor \n"; }
+    virtual void myPrint() { std::cout << "This is base \n"; }
+    virtual void myPure() = 0;
+    virtual ~Base() { std::cout << "Base deconstructor \n"; }
+};
+
+class Derived : public Base
+{
+public:
+    Derived() { std::cout << "Derived constructor \n"; }
+    void myPrint() override { std::cout << "This is Derived \n"; }
+    void myPure() override { std::cout << "This is pure Derived \n"; }
+    ~Derived() override { std::cout << "Derived deconstructor \n"; }
 };
 
 int main()
 {
-   std::cout << "Hello, World!" << std::endl;
-   return 0;
+    Base *base = new Derived();
+    base->myPrint();
+    base->myPure();
+    delete base;
+    return 0;
 }
--- examples/STLIB/main.cpp
+++ examples/STLIB/main.cpp
@@ -1,21 +1,15 @@
 #include <elog/logger.h>
+#include <mylog/log.h>
+#include <mytest/mytest.h>
 
 #include <iostream>
-#include <mytest/mytest.h>
-#include <mylog/log.h>
 using namespace elog;
-
-class Base
-{
-public:
-   Base() {}
-};
 
 int main()
 {
-   log_info("hello world!\n");
-   my_test();
-   Log::info("hello, world!");
-   std::cout << "Hello, World!" << std::endl;
-   return 0;
+    log_info("hello world!\n");
+    my_test();
+    Log::info("hello, world!");
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
